94528c1909dd843b981181beed998dfd441fb244
Alexandre Garrefa <alexmrg@gmail.com>
2015-05-11 15:35:24 -0300

    Project renamed, categories and methods renamed and prefixed

---
+++ Pod/Classes/CSCurrencyParser.h
@@ -0,0 +1,15 @@
+//
+//  CSCurrencyParser.h
+//  Pods
+//
+//  Created by Alexandre Garrefa on 5/11/15.
+//
+//
+
+#ifndef Pods_CSCurrencyParser_h
+#define Pods_CSCurrencyParser_h
+
+#import "CSCurrencyUtil.h"
+#import "NSString+CSCurrencyUtils.h"
+
+#endif
